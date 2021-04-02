92eb4c4a3ff9bca381774e5ef33285f568f512a3
rbrazell1 <brazellrussell@gmail.com>
2021-04-01 21:02:54 -0600

    starting to get google maps working

--- app/src/main/java/edu/cnm/deepdive/sipandscore/controller/BarMapFragment.java
+++ app/src/main/java/edu/cnm/deepdive/sipandscore/controller/BarMapFragment.java
@@ -1,8 +1,38 @@
 package edu.cnm.deepdive.sipandscore.controller;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import edu.cnm.deepdive.sipandscore.databinding.FragmentBarMapBinding;
 
-public class BarMapFragment extends Fragment {
+public class BarMapFragment extends Fragment implements OnMapReadyCallback {
 
+  private FragmentBarMapBinding binding;
 
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = FragmentBarMapBinding.inflate(inflater, container, false);
+    binding.map.getMapAsync(this);
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    // Add a marker in Sydney and move the camera
+    LatLng sydney = new LatLng(-34, 151);
+    googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+    googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+  }
 }
