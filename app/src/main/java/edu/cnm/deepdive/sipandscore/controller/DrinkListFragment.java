66a65ad19875905594bc64b9ddda537bb5e828e0
rbrazell1 <brazellrussell@gmail.com>
2021-04-06 16:17:13 -0600

    builds and runs

--- app/src/main/java/edu/cnm/deepdive/sipandscore/controller/DrinkListFragment.java
+++ app/src/main/java/edu/cnm/deepdive/sipandscore/controller/DrinkListFragment.java
@@ -1,19 +1,27 @@
 package edu.cnm.deepdive.sipandscore.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import edu.cnm.deepdive.sipandscore.R;
+import edu.cnm.deepdive.sipandscore.adapter.DrinkAdapter;
+import edu.cnm.deepdive.sipandscore.adapter.DrinkAdapter.OnDrinkListClickHelper;
 import edu.cnm.deepdive.sipandscore.databinding.FragmentDrinkListBinding;
 import edu.cnm.deepdive.sipandscore.viewmodel.DrinkViewModel;
 
-public class DrinkListFragment extends Fragment {
+public class DrinkListFragment extends Fragment implements OnDrinkListClickHelper {
 
+  private static final int PICK_IMAGE_REQUEST = 0210;
   private FragmentDrinkListBinding binding;
+  private OnDrinkListClickHelper drinkClicker;
   DrinkViewModel drinkViewModel;
+  DrinkAdapter drinkAdapter;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -25,15 +33,30 @@ public class DrinkListFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     binding = FragmentDrinkListBinding.inflate(inflater, container, false);
-    binding.addDrink.setOnClickListener((v) -> {
-      DrinkListFragmentDirections.OpenDrinkDetails action = DrinkListFragmentDirections.openDrinkDetails(0);
-      Navigation.findNavController(binding.getRoot()).navigate(action);
-    });
+    binding.drinkRecyclerView.setAdapter(drinkAdapter);
+    binding.addDrink.setOnClickListener((v) -> pickImage());
     return binding.getRoot();
   }
 
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    drinkViewModel = new ViewModelProvider(getActivity()).get(DrinkViewModel.class);
+  }
 
+  private void pickImage() {
+    Intent intent = new Intent();
+    intent.setType("image/*");
+    intent.setAction(Intent.ACTION_GET_CONTENT);
+    startActivityForResult(Intent.createChooser(intent,
+        getString(R.string.pick_image)),
+        PICK_IMAGE_REQUEST);
+  }
+
+  @Override
+  public void onDrinkClick(long id, View view) {
+    DrinkListFragmentDirections.OpenDrinkDetails openDrinkDetails =
+        DrinkListFragmentDirections.openDrinkDetails(id);
+    openDrinkDetails.setDrinkId(id);
+    Navigation.findNavController(view).navigate(openDrinkDetails);
   }
 }
--- app/src/main/java/edu/cnm/deepdive/sipandscore/viewmodel/DrinkViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/sipandscore/viewmodel/DrinkViewModel.java
@@ -3,14 +3,50 @@ package edu.cnm.deepdive.sipandscore.viewmodel;
 import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import edu.cnm.deepdive.sipandscore.model.entity.Drink;
+import edu.cnm.deepdive.sipandscore.service.DrinkRepository;
+import io.reactivex.disposables.CompositeDisposable;
+import java.util.Date;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
-public class DrinkViewModel extends AndroidViewModel{
+public class DrinkViewModel extends AndroidViewModel implements LifecycleObserver {
+  
+  private final DrinkRepository drinkRepository;
+  private final MutableLiveData<Drink> drink;
+  private final MutableLiveData<List<Drink>> drinkList;
+  private final CompositeDisposable pending;
+  private final MutableLiveData<Throwable> throwable;
+  
 
   public DrinkViewModel drinkViewModel;
 
   public DrinkViewModel(@NonNull Application application) {
     super(application);
+    drinkRepository = new DrinkRepository(application);
+    drink = new MutableLiveData<>();
+    drinkList = new MutableLiveData<>();
+    pending = new CompositeDisposable();
+    throwable = new MutableLiveData<>();
+  }
+
+  public LiveData<Drink> getDrink() {
+    return drink;
+  }
+
+  public LiveData<List<Drink>> getDrinkList() {
+    return drinkList;
+  }
+
+  public LiveData<Throwable> getThrowable() {
+    return throwable;
+  }
+
+  public void store(long id, String name) {
+    throwable.postValue(null);
   }
 }
